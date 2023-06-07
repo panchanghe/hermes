eabd9bb719fd991f2b54a210b5c7e510c8459e2b
pch <panchanghe@fenxianglife.com>
2023-06-07 10:03:09 +0800

    opt

--- hermes-serialization/hermes-serialization-kryo/src/main/java/top/javap/hermes/serialize/kryo/KryoHolder.java
+++ hermes-serialization/hermes-serialization-kryo/src/main/java/top/javap/hermes/serialize/kryo/KryoHolder.java
@@ -1,18 +1,37 @@
 package top.javap.hermes.serialize.kryo;
 
 import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.io.Input;
-import com.esotericsoftware.kryo.io.Output;
 import com.esotericsoftware.kryo.serializers.DefaultSerializers;
 import com.esotericsoftware.kryo.serializers.JavaSerializer;
-import de.javakaffee.kryoserializers.*;
+import de.javakaffee.kryoserializers.ArraysAsListSerializer;
+import de.javakaffee.kryoserializers.BitSetSerializer;
+import de.javakaffee.kryoserializers.GregorianCalendarSerializer;
+import de.javakaffee.kryoserializers.JdkProxySerializer;
+import de.javakaffee.kryoserializers.RegexSerializer;
+import de.javakaffee.kryoserializers.SynchronizedCollectionsSerializer;
+import de.javakaffee.kryoserializers.URISerializer;
+import de.javakaffee.kryoserializers.UUIDSerializer;
+import de.javakaffee.kryoserializers.UnmodifiableCollectionsSerializer;
 
 import java.lang.reflect.InvocationHandler;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.net.URI;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.TreeSet;
+import java.util.UUID;
+import java.util.Vector;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Pattern;
 
@@ -74,21 +93,5 @@ public final class KryoHolder {
 
     public static Kryo get() {
         return ectype.get();
-    }
-
-    public static void main(String[] args) {
-        Output output = new Output(2048);
-        get().writeObjectOrNull(output, "admin", String.class);
-//        get().writeObjectOrNull(output, map, Map.class);
-        get().writeObjectOrNull(output, null, Object.class);
-        get().writeObjectOrNull(output, 123, int.class);
-        byte[] bytes = output.toBytes();
-        Input input = new Input(bytes);
-        String s = get().readObjectOrNull(input, String.class);
-        HashMap map1 = get().readObjectOrNull(input, HashMap.class);
-        Integer a = get().readObjectOrNull(input, int.class);
-        System.err.println(s);
-        System.err.println(map1);
-        System.err.println(a);
     }
 }
